028280a8a8bccef37b9455885fd2cddeaaf805e1
anton <lekennedyon@gmail.com>
2019-11-07 20:58:21 +0200

    Test CI

--- src/components/Markup/index.js
+++ src/components/Markup/index.js
@@ -5,14 +5,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Fade } from '@material-ui/core';
-import { Route, Switch } from 'react-router-dom';
+// import { Route, Switch } from 'react-router-dom';
+// // import Background from '../Background';
+// import Opening from './Opening';
+// import Genre from './Genre';
+// import RouteGallery from './RouteGallery';
+// import Language from '../Language';
+// import ErrorPage from './ErrorPage';
 // import Background from '../Background';
-import Opening from './Opening';
-import Genre from './Genre';
-import RouteGallery from './RouteGallery';
-import Language from '../Language';
-import ErrorPage from './ErrorPage';
-import Background from '../Background';
 
 class Markup extends Component {
   render() {
@@ -20,14 +20,14 @@ class Markup extends Component {
     return (
         <Fade in={!loading} timeout={{ exit: 800, enter: 400 }}>
             <div className='Markup'>
-                <Background />
+                {/* <Background />
                 <Language />
                 <Switch>
                     <Route exact path='/' render={() => <Opening />} />
                     <Route exact path='/genre' render={() => <Genre />} />
                     <Route exact path='/:name' render={(props) => <RouteGallery {...props} />} />
                     <Route path='*' render={() => <ErrorPage />} />
-                </Switch>
+                </Switch> */}
             </div>
         </Fade>
     );
